fad678094d63e0fdf7059b6bd56fbdc65c100353
Vigneshram0607 <vigneshram0706@gmail.com>
2025-12-12 20:48:40 +0530

    added controller

--- src/main/java/com/ai/research_assistant/controller/ResearchController.java
+++ src/main/java/com/ai/research_assistant/controller/ResearchController.java
@@ -1,12 +1,15 @@
-package com.ai.research_assistant;
+package com.ai.research_assistant.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ai.research_assistant.service.ResearchService;
+import com.ai.research_assistant.entity.ResearchRequest;
 
 import lombok.AllArgsConstructor;
 
@@ -20,8 +23,10 @@ public class ResearchController {
     private final ResearchService researchService;
 
     @PostMapping("/process")
-    public ResponseEntity<String> processContent(){
-        return new ResponseEntity<>("null",HttpStatus.OK);
+    public ResponseEntity<String> processContent(@RequestBody ResearchRequest request){
+        String result = researchService.processContent(request);
+        // return new ResponseEntity<>("null",HttpStatus.OK);
+        return ResponseEntity.ok(result);
     }
 
 }
